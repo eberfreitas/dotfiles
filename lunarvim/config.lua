0c5d1aea18ea21840ad8bc301424452599fd9ec3
Éber Freitas Dias <eber.freitas@gmail.com>
2024-02-14 16:09:02 -0300

    Moar plugins

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -4,9 +4,37 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 lvim.plugins = {
   { "tpope/vim-sleuth" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "folke/todo-comments.nvim",
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
+  {
+    "ggandor/leap.nvim",
+    name = "leap",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
 }
 
 lvim.colorscheme = "catppuccin-frappe"
 
 lvim.format_on_save.enabled = true
+
+lvim.builtin.which_key.mappings["t"] = {
+  name = "Diagnostics",
+  t = { "<cmd>TroubleToggle<cr>", "trouble" },
+  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+  q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+  l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+  r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+}
