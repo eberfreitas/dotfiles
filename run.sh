c399011f0420bdcc1479113bec2afc490d03135f
Éber Freitas Dias <eber.freitas@gmail.com>
2024-02-14 11:24:57 -0300

    Lunarvim configs

--- run.sh
+++ run.sh
@@ -10,24 +10,25 @@ manage_symlink() {
 
     if [ -e "$target_path" ]; then
         if [ -L "$target_path" ] && [ "$(readlink "$target_path")" = "$source_file" ]; then
-            echo "** symlink at $target_path already points to $source_file. doing nothing..."
+            echo "**** symlink at $target_path already points to $source_file. doing nothing..."
         else
             if [ -L "$target_path" ]; then
-                echo "** replacing existing symlink at $target_path with $source_file"
+                echo "**** replacing existing symlink at $target_path with $source_file"
             else
-                echo "** backing up existing file at $target_path to $target_path.bak"
+                echo "**** backing up existing file at $target_path to $target_path.bak"
             fi
 
             mv "$target_path" "$target_path.bak"
             ln -s "$source_file" "$target_path"
 
-            echo "** symlink created at $target_path"
+            echo "**** symlink created at $target_path"
         fi
     else
         ln -s "$source_file" "$target_path"
-        echo "** symlink created at $target_path"
+        echo "**** symlink created at $target_path"
     fi
 }
 
 manage_symlink "$HOME/.gitconfig" "$SCRIPT_DIR/git/.gitconfig"
 manage_symlink "$HOME/.tmux.conf" "$SCRIPT_DIR/tmux/.tmux.conf"
+manage_symlink "$HOME/.config/lvim/config.lua" "$SCRIPT_DIR/lunarvim/config.lua"
