111930a9ba72d5a9c06a5c60cc497f99619949b4
Éber Freitas Dias <eber.freitas@gmail.com>
2024-02-14 11:04:58 -0300

    Reduces scope to only manage symlinks

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-ASDF_VERSION=v0.14.0
 
 manage_symlink() {
     local target_path="$1"
@@ -28,71 +27,4 @@ manage_symlink() {
     fi
 }
 
-update() {
-    echo "** updating system packages..."
-    sudo apt update && sudo apt upgrade -y
-}
-
-install_essentials() {
-    echo "** installing essential software..."
-    sudo apt install curl git tmux
-}
-
-setup_zsh() {
-    if [ -z "$(command -v zsh)" ]; then
-        echo "** zsh is not installed. installing..."
-        sudo apt install zsh -y
-    else
-        echo "** zsh is already installed!"
-    fi
-
-    if [ "$SHELL" != "$(which zsh)" ]; then
-        chsh -s "$(which zsh)"
-        echo "** zsh is now the default shell. enter zsh and re-run this script."
-        exit 0
-    else
-        echo "** zsh is already the default shell."
-    fi
-}
-
-setup_omz() {
-    if [ ! -d "$HOME/.oh-my-zsh" ]; then
-        echo "** omz is not installed. installing..."
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    else
-        echo "** omz is already installed. updating..."
-        "$HOME/.oh-my-zsh/tools/upgrade.sh"
-    fi
-}
-
-setup_asdf() {
-    if [ ! -d "$HOME/.asdf" ]; then
-        echo "** asdf is not installed. installing..."
-        git clone https://github.com/asdf-vm/asdf.git "$HOME/.asdf" --branch "$ASDF_VERSION"
-        sed -i.bak '/^plugins=(/ s/)$/ asdf)/' "$HOME/.zshrc"
-    else
-        echo "** asdf is already installed. updating..."
-        asdf update
-        echo "** updating asdf plugins..."
-        asdf plugin update --all
-    fi
-}
-
-setup_tpm() {
-    tmux_plugins_dir="$HOME/.tmux/plugins/tpm"
-
-    if [ ! -d "$tmux_plugins_dir" ]; then
-        echo "** tpm is not installed. installing..."
-        git clone https://github.com/tmux-plugins/tpm "$tmux_plugins_dir"
-    else
-        echo "** tpm is already installed."
-    fi
-}
-
-update
-install_essentials
 manage_symlink "$HOME/.gitconfig" "$SCRIPT_DIR/git/.gitconfig"
-setup_zsh
-setup_omz
-setup_asdf
-setup_tpm
